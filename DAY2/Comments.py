c0d7df8cf3c703cd4fa6279bfb3e97cc309ee989
prabeshkhatiwada <prabeshkhatiwada29@gmail.com>
2024-06-04 20:08:55 +0545

    Python

--- DAY2/Comments.py
+++ DAY2/Comments.py
@@ -1,1 +1,29 @@
 # In comments we are wite the code in standard form
+# Author=Prabesh Khatiwada
+# course= 100 days of coding
+# \n is the escape sequence
+print("Hey I am a god Boy \nand this boy is a god boy")
+print("Hello world")
+
+
+print("Hello world") # printing hello world
+
+
+# multilne comments
+
+
+#prabesh
+# khatiwada is a coder 
+
+
+
+'''
+prabesh
+'''
+"""
+khatiwada
+"""
+
+# escape sequence
+
+print('hey i am \'a\"good boy\"\n and this viewer is also a good boy')
---
+++ Day 3/Data Types.py
@@ -0,0 +1,37 @@
+# data types hold the variable data types
+
+
+a=1
+b="Prabesh kahatiwada"
+c=1.222
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+x = 5
+print(type(x))
+
+
+
+x=["apple","banana","cherry"]
+# display x
+print(x)
+print(type(x))
+
+
+
+x = (("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+x=True
+print(x)
+
+print(type(x))
