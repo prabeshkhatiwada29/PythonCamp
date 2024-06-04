cb725a1a6cfd2c8ab3f8b7eb4b3514bd7f3f09e6
prabeshkhatiwada <prabeshkhatiwada29@gmail.com>
2024-06-04 14:01:38 +0545

    Python

---
+++ DAY 1/basic.py
@@ -0,0 +1,100 @@
+# # # # # # # # # x=12+"prabesh"
+# # # # # # # # # print(x)
+
+
+# # # # # # # # x="babu"
+# # # # # # # # print("my name is ",x)
+# # # # # # # x=int(1)
+# # # # # # # y=int(1.2)
+# # # # # # # z=int(1.333)
+# # # # # # # print(y)
+# # # # # # # print(x,y,z)
+
+
+
+# # # # # # # x=str(1)
+# # # # # # # y="word"
+# # # # # # # print(x+y)
+
+
+# # # # # # list
+# # # # # # x=[1,2,3, 'word','house']
+# # # # # # y=x+'cow'
+# # # # # # print(y)
+# # # # # # print(x)
+
+# # # # # tupple
+
+# # # # x=(1,2,3,4,'house','cow')
+# # # # print(x)
+
+
+# # # set
+# # # x={1,2,3,4,5,'cow','dog'}
+# # # print(x)
+
+
+
+# # x={'a':'apple','b':'ball','a':'abb'}
+# # print(x)
+
+# # name="prabesh"
+# # age=12.0
+# # is_adult=True
+# # print(name)
+# # print(age)
+
+# # # userInput
+# # name=input("What is your name?")
+# # print("Hello"+name)
+
+
+# # Type conversion
+
+# old_age=input("Enter your old age:")
+# new_age=int(old_age)+2
+# new_age=old_age+2
+# print(new_age)
+
+
+# first=input("Enter first number")
+# second=input("Enter second number")
+# sum=int(first)+int(second)
+# print("the sum is:"+str(sum))
+
+
+
+# name="prabesh khatiwada"
+
+
+
+# print(name.find("t"))
+# print(name.replace("prabesh khatiwada","pk"))
+# print(name)
+
+
+
+
+# a=5
+# b=6
+# if(a<b):
+#    print("a is smaller")
+# else:
+#     print("b is greater")
+
+
+
+
+# a=input("Enter firts number")
+# b=input("Enter a second number")
+
+# sum=int(a)+int(b)
+# print("Sum is " ,sum)
+
+
+
+# a=int(input("Enter a no"))
+# b=int(input("Enter 2nd number"))
+# sum=a+b
+# print("sum is",sum)
+
